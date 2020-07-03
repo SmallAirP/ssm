1b1cf0aa9c397aee02ad8f9510cd0d33a9d25b62
fanrui <860923739@qq.com>
2020-07-03 22:15:45 +0800

    短信验证接口

---
+++ alimessqge/src/main/java/MessageImpl.java
@@ -0,0 +1,40 @@
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+
+public class MessageImpl implements IMessage{
+    public void sendMessage(String phoneNumbers, String templateParam) {
+        String accessKeyId = "";
+        String accessSecret = "";
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setSysMethod(MethodType.POST);
+        request.setSysDomain("dysmsapi.aliyuncs.com");
+        request.setSysVersion("2017-05-25");
+        request.setSysAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        //短信接收号码
+        request.putQueryParameter("PhoneNumbers", phoneNumbers);
+        //短信签名名称
+        request.putQueryParameter("SignName", "xxx");
+        //短信模板ID
+        request.putQueryParameter("TemplateCode", "SMS_195220796");
+        //验证码
+        request.putQueryParameter("TemplateParam", "{\"code\":"+templateParam+"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
